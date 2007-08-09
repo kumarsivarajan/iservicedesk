0e17e5e05a0306714746bdbf325290b13e4c1604
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-09 08:16:09 +0000

    RefInspectableEntityObject

--- trunk/iservicedesk/src/cn/iservicedesk/infrastructure/RefInspectableEntityObject.java
+++ trunk/iservicedesk/src/cn/iservicedesk/infrastructure/RefInspectableEntityObject.java
@@ -1,5 +1,9 @@
 package cn.iservicedesk.infrastructure;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * 可以检查引用的 Entity
  * 采用 Property 格式，如：{ACCOUNT=1,2,3,4 USER=12,34,56}
@@ -9,6 +13,23 @@ package cn.iservicedesk.infrastructure;
 public class RefInspectableEntityObject extends EntityObject{
     
     protected String refs;
+
+    private Map<String, List<String>> refsMap = new HashMap<String, List<String>>();
+
+    public void setRefs(String refs) {
+        this.refs = refs;
+        //TODO: compute refs string 
+    }
+
+    public String getRefs(){
+        //TODO: need to recompute refs string
+        return refs;
+    }
+
+    public String[] getReferenceIds(String key){
+        List<String> refIds = refsMap.get(key);
+        return refIds.toArray(new String[refIds.size()]);
+    }
 
     public static void main(String[] args) {
 
