2d536d8647ace3428039ef602816720ee7b76b49
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-09 07:05:51 +0000

    SuperAction 增加多语言

--- trunk/iservicedesk/src/cn/iservicedesk/function/entity/Function.java
+++ trunk/iservicedesk/src/cn/iservicedesk/function/entity/Function.java
@@ -1,12 +1,46 @@
 package cn.iservicedesk.function.entity;
 
+import java.util.Locale;
+import java.util.Properties;
+import java.io.StringReader;
+import java.io.ByteArrayInputStream;
+
 /**
  * @author <a href="mailto:jfox.young@gmail.com">Young Yang</a>
  */
 public class Function {
 
-    // 多语言 properties 字符串
-    private String multilang;
+    private String name;
+    //localName 多语言 properties 字符串
+    private String localName = "";
+    private Properties localNameProperties = null;
+
+    public String getLocalName(Locale locale) {
+        return getLocalName(locale.getDisplayName());
+    }
+
+    public String getLocalName(String localeName) {
+        Properties localNameProperties = getLocalNameProperties();
+        if (localNameProperties.contains(localeName)) {
+            return localNameProperties.getProperty(localeName);
+        }
+        else {
+            return name;
+        }
+    }
+
+    private Properties getLocalNameProperties() {
+        if (localNameProperties == null) {
+            localNameProperties = new Properties();
+            try {
+                localNameProperties.load(new ByteArrayInputStream(localName.getBytes()));
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return localNameProperties;
+    }
 
     public static void main(String[] args) {
 
