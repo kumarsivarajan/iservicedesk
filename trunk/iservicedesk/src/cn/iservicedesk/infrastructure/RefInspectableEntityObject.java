f6ffa7d4f6ea9ad2a2bea795ba476c6f8285fc85
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-09 08:07:58 +0000

    RefInspectableEntityObject

--- trunk/iservicedesk/src/cn/iservicedesk/infrastructure/RefInspectableEntityObject.java
+++ trunk/iservicedesk/src/cn/iservicedesk/infrastructure/RefInspectableEntityObject.java
@@ -2,6 +2,7 @@ package cn.iservicedesk.infrastructure;
 
 /**
  * 可以检查引用的 Entity
+ * 采用 Property 格式，如：{ACCOUNT=1,2,3,4 USER=12,34,56}
  *
  * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
  */
