0373c56e5531c1bbed9c3ecb1efdcc11ba7f67aa
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-09 08:05:02 +0000

    RefInspectableEntityObject

---
+++ trunk/iservicedesk/src/cn/iservicedesk/infrastructure/RefInspectableEntityObject.java
@@ -0,0 +1,15 @@
+package cn.iservicedesk.infrastructure;
+
+/**
+ * 可以检查引用的 Entity
+ *
+ * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
+ */
+public class RefInspectableEntityObject extends EntityObject{
+    
+    protected String refs;
+
+    public static void main(String[] args) {
+
+    }
+}
