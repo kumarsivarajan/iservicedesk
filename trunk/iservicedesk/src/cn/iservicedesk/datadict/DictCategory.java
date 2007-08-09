c913b80601ce71653ed67d20cd4ee7898b4439c4
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-09 07:40:24 +0000

    SuperAction 增加多语言

---
+++ trunk/iservicedesk/src/cn/iservicedesk/datadict/DictCategory.java
@@ -0,0 +1,13 @@
+package cn.iservicedesk.datadict;
+
+import cn.iservicedesk.infrastructure.LocalNameEntityObject;
+
+/**
+ * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
+ */
+public class DictCategory extends LocalNameEntityObject {
+
+    public static void main(String[] args) {
+
+    }
+}
