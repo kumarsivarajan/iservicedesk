b9660601f051686b9e9afdcbabc277f14cca80f8
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-10 05:50:09 +0000

    RefInspectableEntityObject

--- trunk/iservicedesk/src/cn/iservicedesk/infrastructure/RefInspectableEntityObject.java
+++ trunk/iservicedesk/src/cn/iservicedesk/infrastructure/RefInspectableEntityObject.java
@@ -1,8 +1,11 @@
 package cn.iservicedesk.infrastructure;
 
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import javax.persistence.Column;
 
 /**
@@ -11,37 +14,64 @@ import javax.persistence.Column;
  *
  * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
  */
-public abstract class RefInspectableEntityObject extends EntityObject{
+public abstract class RefInspectableEntityObject extends EntityObject {
     // 是否已经初始化 refs
     private boolean refsInit = false;
 
-    @Column(name="REFS")
+    @Column(name = "REFS")
     protected String refs;
 
-    private Map<String, List<String>> refsMap = new HashMap<String, List<String>>();
+    private Map<String, List<Long>> refsMap = new HashMap<String, List<Long>>();
 
     public void setRefs(String refs) {
         this.refs = refs;
         //TODO: compute refs string 
     }
 
-    public String getRefs(){
+    public String getRefs() {
         //TODO: need to recompute refs string
         return refs;
     }
 
-    public boolean isReferenced(){
+    public boolean isReferenced() {
         return !refsMap.isEmpty();
     }
 
     //TODO: 
-    public void addReference(String tableName, long entityId) {
-        
+    public synchronized void addReference(String tableName, long entityId) {
+        if (!refsInit) {
+            if (refs != null && refs.trim().length() > 0) {
+                Properties refProperties = new Properties();
+                try {
+                    refProperties.load(new ByteArrayInputStream(refs.getBytes()));
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+                for(Map.Entry<Object, Object> entry : refProperties.entrySet()){
+                    String tabName = entry.getKey().toString();
+                    String value = entry.getValue().toString();
+                    String[] refIds = value.split(",");
+                    List<Long> refIdList = new ArrayList<Long>(refIds.length);
+                    for(String refId : refIds){
+                        refIdList.add(Long.parseLong(refId));
+                    }
+                    refsMap.put(tabName,refIdList);
+                }
+            }
+            refsInit = true;
+
+            List<Long> refIds = refsMap.get(tableName);
+            if(refIds == null){
+                refIds = new ArrayList<Long>(1);
+            }
+            refIds.add(entityId);
+        }
     }
 
-    public String[] getReferenceIds(String key){
-        List<String> refIds = refsMap.get(key);
-        return refIds.toArray(new String[refIds.size()]);
+    public Long[] getReferenceIds(String key) {
+        List<Long> refIds = refsMap.get(key);
+        return refIds.toArray(new Long[refIds.size()]);
     }
 
     public static void main(String[] args) {
