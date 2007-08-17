87a416972f69ec619b134014f7206fa724a7500e
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-17 16:52:23 +0000

    使用 explode 方式，解决了Debug修改类和vhtml无需重启的问题

--- trunk/iservicedesk/src/cn/iservicedesk/function/action/NodeAction.java
+++ trunk/iservicedesk/src/cn/iservicedesk/function/action/NodeAction.java
@@ -23,7 +23,7 @@ public class NodeAction extends SuperAction {
     @ActionMethod(name="new", successView = "function/new_node.vhtml")
     public void newFunction(InvocationContext invocationContext) throws Exception {
 
-        System.out.println("!!! Hello, " + invocationContext.getFullActionMethodName());
+//        System.out.println("!!! Hello, " + invocationContext.getFullActionMethodName());
 
     }
 
