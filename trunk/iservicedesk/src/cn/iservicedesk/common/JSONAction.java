6c806abad4ca8b5ef7db791b5a5e0ad5923a45cc
yy.young <yy.young@7dc5693e-9036-0410-a58b-ad421088f1dd>
2007-08-21 01:51:51 +0000

    add json

--- trunk/iservicedesk/src/cn/iservicedesk/common/JSONAction.java
+++ trunk/iservicedesk/src/cn/iservicedesk/common/JSONAction.java
@@ -5,9 +5,11 @@ import org.jfox.framework.annotation.Service;
 import org.jfox.mvc.InvocationContext;
 
 /**
+ * 所有异步请求，都通过这个 Action 执行
+ *
  * @author <a href="mailto:yang_y@sysnet.com.cn">Young Yang</a>
  */
-@Service(id="json")
+@Service(id="async")
 public class JSONAction extends SuperAction {
 
     public void createModule(InvocationContext invocationContext) throws Exception {
